1eef72eb2932fde32e9025b6055110a509ba72a3
shivakumarchirra <chirrashivakumar5@gmail.com>
2025-06-05 10:39:27 +0530

    added by shiva

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import logo from './logo.svg';
+
 import './App.css';
 
 function App() {
@@ -6,6 +6,41 @@ function App() {
     <div className="App">
       <h1>This line is added by Shiva</h1>
       <h1>This line is added by Manjunadh</h1>
+      <p> this is shiva . i have changed the code that that adding the some of the data about the git and remote repositores </p>
+    <div className='card'>
+    <h1>Today Session</h1>
+    <hr></hr>
+    <h2>repositores</h2>
+    <hr></hr>
+    <h5>local repositories</h5>
+    <hr></hr>
+    <p> A repository that can stored hiiden in  the local folder that can store the git location and also the data of that project . its only allow to the personal computer user to grab that details except none another. here no one can get the porject details  </p>
+    <h5>remote repository</h5>
+    <hr></hr>
+    <p> a remote repository is the globally like a internet access type that can anyone in the world who has git can see your code except you put that in  the remote repository and with a public repo and main thing is is can any one will just get the project but they cant even push the code into the main repositry i meean remote repository </p>
+    <h2> clone</h2>
+    <hr></hr>
+    <p> a clone is the step that in the repositories that can get the url of the main repository so when ever we need the remote repository we get using the url  </p>
+    <h2>push</h2>
+    <hr></hr>
+    <p> A push is a key work that can push the local code into the main repository that can be updated in the git website </p>
+    <h2>pull</h2>
+    <hr></hr>
+    <p> a pull is a key word that can be pulled from the git to the local device using the pull and the url</p>
+    <h3> Advantages </h3>
+    <hr></hr>
+    <ul>
+      <li>1. can sharable trough the entire country</li>
+      <li> 2. can anyone get the code when it is as a public repo </li>
+    
+    </ul>
+    <h3>disadvantages </h3>
+    <hr></hr>
+   <ul>
+    <li>1. if there is no git then we cant get the project details</li>
+    <li> 2 . we must need to check the current repos is the updated repo or not otherwise it won't be pushed to the remote repository </li>
+   </ul>
+    </div>
     </div>
   );
 }
